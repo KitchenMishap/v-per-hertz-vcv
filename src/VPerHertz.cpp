acdf946f4a4937a253b532081c46c04546253134
KitchenMishap <hello@kitchenmishap.art>
2025-10-29 07:38:14 +0000

    Do the conversion & calibration. Spell CALIBRATEDD_LIGHT correctly

--- src/VPerHertz.cpp
+++ src/VPerHertz.cpp
@@ -5,6 +5,21 @@ void runTests()
 	assert(true);
 }
 
+const float C4Hz = 256.f;
+
+float VhzFromVoct(float voct, float f0)
+{
+	float f = pow(2.f, voct) * C4Hz;
+	float vhz = f / f0 - 1.f;
+	return vhz;
+}
+
+float VoctFromVhz(float vhz, float f0)
+{
+	float f = f0 * (1.f + vhz);
+	float voct = (log(f) - log(256.f)) / log(2.f);
+    return voct;
+}
 
 struct VPerHertz : Module {
 	enum ParamId {
@@ -27,9 +42,11 @@ struct VPerHertz : Module {
 		OUTPUTS_LEN
 	};
 	enum LightId {
-		CALIBRATEDD_LIGHT,
+		CALIBRATED_LIGHT,
 		LIGHTS_LEN
 	};
+
+	float oldCalButton = 0.f;
 
 	VPerHertz() {
 		INFO("ToDo: Running Tests...");
@@ -44,20 +61,43 @@ struct VPerHertz : Module {
 		configOutput(VHZ1_OUTPUT, "V/Hz 1");
 		configInput(VOCT2_INPUT, "V/Oct 2");
 		configOutput(VHZ2_OUTPUT, "V/Hz 2");
+
+		oldCalButton = 0.f;
 	}
 
 
 	void process(const ProcessArgs& args) override {
-		// Example: Set polyphonic output equal to input
-		//int numPoly = inputs[TODO_INPUT].getChannels();
-		//outputs[TODO_OUTPUT].setChannels(numPoly);
-		//for( int i=0; i<numPoly; i++ ) {
-		//	float inputV = inputs[TODO_INPUT].getVoltage(i);
-		//	outputs[TODO_OUTPUT].setVoltage(inputV, i);
-		//}
-		// Example: Turn LED on
-		//lights[TODO_LIGHT].setBrightness(1.f);
-	}
+		float calButton = params[CALIBRATE_PARAM].getValue();
+		if( calButton == 1.f && oldCalButton == 0.f )
+		{
+			// Perform calibration
+			float vhz = inputs[VHZ1_INPUT].getVoltage();
+			if( vhz > 0.f )
+			{
+				float f0 = 256.f / (1.f + vhz);
+				params[MANUAL_PARAM].setValue(f0);
+				lights[CALIBRATED_LIGHT].setBrightness(1.f);
+			}
+		}
+		oldCalButton = calButton;
+
+		float f0 = params[MANUAL_PARAM].getValue();
+		float vhz = inputs[VHZ1_INPUT].getVoltage();
+		float voct = VoctFromVhz(vhz, f0);
+		outputs[VOCT1_OUTPUT].setVoltage(voct);
+
+		vhz = inputs[VHZ2_INPUT].getVoltage();
+		voct = VoctFromVhz(vhz, f0);
+		outputs[VOCT2_OUTPUT].setVoltage(voct);
+
+		voct = inputs[VOCT1_INPUT].getVoltage();
+		vhz = VhzFromVoct(voct, f0);
+		outputs[VHZ1_OUTPUT].setVoltage(vhz);
+
+		voct = inputs[VOCT2_INPUT].getVoltage();
+		vhz = VhzFromVoct(voct, f0);
+		outputs[VHZ2_OUTPUT].setVoltage(vhz);
+		}
 };
 
 struct VPerHertzWidget : ModuleWidget {
@@ -87,7 +127,7 @@ struct VPerHertzWidget : ModuleWidget {
 		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(7, 113)), module, VPerHertz::VOCT2_INPUT));
 		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(width - 7, 113)), module, VPerHertz::VHZ2_OUTPUT));
 
-		addChild(createLightCentered<MediumLight<GreenLight>>(mm2px(Vec(width - 7, 54)), module, VPerHertz::CALIBRATEDD_LIGHT));
+		addChild(createLightCentered<MediumLight<GreenLight>>(mm2px(Vec(width - 7, 54)), module, VPerHertz::CALIBRATED_LIGHT));
 
 		// mm2px(Vec(10.0, 10.0))
 		addChild(createWidget<Widget>(mm2px(Vec(2.684, 81.945))));
